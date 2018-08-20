6fde9d4a9f3027a6558b99a662fd1eea71696030
Sean Knox <sean.knox@microsoft.com>
2018-08-20 17:04:10 -0500

    iterate over struct fields instead of explicit declaration

--- kube_resources.go
+++ kube_resources.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"reflect"
 
 	"github.com/fatih/color"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -43,50 +44,56 @@ func main() {
 	}
 
 	type Container struct {
-		Name                       string
-		ResourceCPUMissingLimit    bool
-		ResourceMemoryMissingLimit bool
+		Name           string
+		ResourceCPU    bool
+		ResourceMemory bool
+		RequestCPU     bool
+		RequestMemory  bool
 	}
 
 	offenders := make(map[string][]*Container)
 
 	for _, d := range deployments.Items {
-		// fmt.Printf("[%s] %s * %d replicas\n", d.GetNamespace(), d.GetName(), *d.Spec.Replicas)
-		// fmt.Printf("[%s] %s * %s containers\n", d.GetNamespace(), d.GetName(), d.Spec.Template.Spec.Containers)
 		containers := d.Spec.Template.Spec.Containers
 		for _, c := range containers {
-			cpuLimitMissing := false
-			memoryLimitMissing := false
+			var cpuLimitMissing, memoryLimitMissing, cpuRequestMissing, memoryRequestMissing bool
+
 			if c.Resources.Limits.Cpu().IsZero() {
 				cpuLimitMissing = true
 			}
 			if c.Resources.Limits.Memory().IsZero() {
 				memoryLimitMissing = true
 			}
+			if c.Resources.Requests.Cpu().IsZero() {
+				cpuRequestMissing = true
+			}
+			if c.Resources.Requests.Memory().IsZero() {
+				memoryRequestMissing = true
+			}
 			if cpuLimitMissing || memoryLimitMissing {
-				container := Container{c.Name, cpuLimitMissing, memoryLimitMissing}
+				container := Container{c.Name, cpuLimitMissing, memoryLimitMissing, cpuRequestMissing, memoryRequestMissing}
 				offenders[d.GetName()] = append(offenders[d.GetName()], &container)
 			}
 		}
 	}
 
 	fmt.Println("The following containers have no resources limits and may cause node resource starvation: ")
-	for k, v := range offenders {
-		fmt.Println("-----")
+	for k, containers := range offenders {
 		c := color.New(color.FgBlue, color.Underline, color.Bold)
 		c.Printf("Deployment name: %s\n", k)
-		for _, c := range v {
+		for _, c := range containers {
 			cc := color.New(color.Bold)
 			cc.Printf("- Container: %s\n", c.Name)
-			if c.ResourceCPUMissingLimit {
-				color.Red("cpuLimitMissing: %v", c.ResourceCPUMissingLimit)
-			} else {
-				color.Green("cpuLimitMissing: %v", c.ResourceCPUMissingLimit)
-			}
-			if c.ResourceMemoryMissingLimit {
-				color.Red("memoryLimitMissing: %v", c.ResourceMemoryMissingLimit)
-			} else {
-				color.Green("memoryLimitMissing: %v", c.ResourceMemoryMissingLimit)
+			v := reflect.Indirect(reflect.ValueOf(c))
+			// Purposely skip the first struct field, Name, which is a string
+			for i := 1; i < 5; i++ {
+				key := v.Type().Field(i).Name
+				value := v.Field(i).Bool()
+				if value == true {
+					color.Red("%+v has no limit set", key)
+				} else {
+					color.Green("%+v limit is set", key)
+				}
 			}
 		}
 	}
