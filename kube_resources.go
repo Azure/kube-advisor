43230f5aab119dac5b9538c2bec3ccdf35e7632e
Sean Knox <sean.knox@microsoft.com>
2018-08-21 08:57:27 -0500

    support querying statefulsets

--- kube_resources.go
+++ kube_resources.go
@@ -2,20 +2,39 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"reflect"
 
 	"github.com/fatih/color"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-type ContainerStatus struct {
-	Name           string
+func checkContainer(c v1.Container) LimitCheckStatus {
+	var cpuLimitMissing, memoryLimitMissing, cpuRequestMissing, memoryRequestMissing bool
+
+	if c.Resources.Limits.Cpu().IsZero() {
+		cpuLimitMissing = true
+	}
+	if c.Resources.Limits.Memory().IsZero() {
+		memoryLimitMissing = true
+	}
+	if c.Resources.Requests.Cpu().IsZero() {
+		cpuRequestMissing = true
+	}
+	if c.Resources.Requests.Memory().IsZero() {
+		memoryRequestMissing = true
+	}
+	return LimitCheckStatus{c.Name, cpuLimitMissing, memoryLimitMissing, cpuRequestMissing, memoryRequestMissing}
+}
+
+// LimitCheckStatus represents a container and its resource and request limit status
+type LimitCheckStatus struct {
+	ContainerName  string
 	ResourceCPU    bool
 	ResourceMemory bool
 	RequestCPU     bool
@@ -48,74 +67,53 @@ func main() {
 
 	deployments, err := clientset.AppsV1().Deployments("").List(metav1.ListOptions{})
 	daemonsets, err := clientset.AppsV1().DaemonSets("").List(metav1.ListOptions{})
+	statefulsets, err := clientset.AppsV1().StatefulSets("").List(metav1.ListOptions{})
 	if err != nil {
 		log.Fatalln("failed to get deployments:", err)
 	}
 
-	offenders := make(map[string][]*ContainerStatus)
+	statuses := make(map[string][]*LimitCheckStatus)
 
+	// Gather container statuses from Deployments
 	for _, d := range deployments.Items {
 		containers := d.Spec.Template.Spec.Containers
 		for _, c := range containers {
-			var cpuLimitMissing, memoryLimitMissing, cpuRequestMissing, memoryRequestMissing bool
-
-			if c.Resources.Limits.Cpu().IsZero() {
-				cpuLimitMissing = true
-			}
-			if c.Resources.Limits.Memory().IsZero() {
-				memoryLimitMissing = true
-			}
-			if c.Resources.Requests.Cpu().IsZero() {
-				cpuRequestMissing = true
-			}
-			if c.Resources.Requests.Memory().IsZero() {
-				memoryRequestMissing = true
-			}
-			if cpuLimitMissing || memoryLimitMissing {
-				container := ContainerStatus{c.Name, cpuLimitMissing, memoryLimitMissing, cpuRequestMissing, memoryRequestMissing}
-				offenders[d.GetName()] = append(offenders[d.GetName()], &container)
-			}
+			status := checkContainer(c)
+			statuses[d.GetName()] = append(statuses[d.GetName()], &status)
 		}
 	}
 
+	// Gather container statuses from StatefulSets
+	for _, ss := range statefulsets.Items {
+		containers := ss.Spec.Template.Spec.Containers
+		for _, c := range containers {
+			status := checkContainer(c)
+			statuses[ss.GetName()] = append(statuses[ss.GetName()], &status)
+		}
+	}
+
+	// Gather container statuses from DaemonSets
 	for _, ds := range daemonsets.Items {
 		containers := ds.Spec.Template.Spec.Containers
 		for _, c := range containers {
-			var cpuLimitMissing, memoryLimitMissing, cpuRequestMissing, memoryRequestMissing bool
-
-			if c.Resources.Limits.Cpu().IsZero() {
-				cpuLimitMissing = true
-			}
-			if c.Resources.Limits.Memory().IsZero() {
-				memoryLimitMissing = true
-			}
-			if c.Resources.Requests.Cpu().IsZero() {
-				cpuRequestMissing = true
-			}
-			if c.Resources.Requests.Memory().IsZero() {
-				memoryRequestMissing = true
-			}
-			if cpuLimitMissing || memoryLimitMissing {
-				container := ContainerStatus{c.Name, cpuLimitMissing, memoryLimitMissing, cpuRequestMissing, memoryRequestMissing}
-				offenders[ds.GetName()] = append(offenders[ds.GetName()], &container)
-			}
+			status := checkContainer(c)
+			statuses[ds.GetName()] = append(statuses[ds.GetName()], &status)
 		}
 	}
 
-	fmt.Println("The following containers have no resources limits and may cause node resource starvation: ")
-	for k, containers := range offenders {
+	for k, limitStatuses := range statuses {
 		c := color.New(color.FgBlue, color.Underline, color.Bold)
-		c.Printf("Deployment/Daemon Set name: %s\n", k)
-		for _, c := range containers {
+		c.Printf("Deployment/DaemonSet/StatefulSet name: %s\n", k)
+		for _, s := range limitStatuses {
 			cc := color.New(color.Bold)
-			cc.Printf("Container: %s\n", c.Name)
-			v := reflect.Indirect(reflect.ValueOf(c))
+			cc.Printf("Container: %s\n", s.ContainerName)
+			v := reflect.Indirect(reflect.ValueOf(s))
 			// Purposely skip the first struct field, Name, which is a string
 			for i := 1; i < 5; i++ {
 				key := v.Type().Field(i).Name
 				value := v.Field(i).Bool()
 				if value == true {
-					color.Red("- %+v has no limit set", key)
+					color.Red("- %+v has no limit set and may cause node resource starvation", key)
 				} else {
 					color.Green("- %+v limit is set", key)
 				}
